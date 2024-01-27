1be8e354e63992faa65674b01169c3cc5d9a2f22
Randy Moya <stevenmoya02@gmail.com>
2024-01-26 21:33:04 -0600

    Se agrego estado al componente TasksSearch

--- src/TasksSearch.js
+++ src/TasksSearch.js
@@ -1,11 +1,18 @@
-import './TasksSearch.css'
+import React from 'react';
+import './TasksSearch.css';
+
+
 
 function TasksSearch(){
+  const [searchValue, setSearchValue] = React.useState('');
+
+
   return(
-    <input className='input-search' placeholder="Do my english homework"
+    <input className='input-search' placeholder="Do my english homework" value={searchValue}
     onChange={(e)=>{
       console.log("Writting in the input");
       console.log(e.target.value);
+      setSearchValue(e.target.value);
     }}
     ></input>
   );
