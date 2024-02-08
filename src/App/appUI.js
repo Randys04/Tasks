2208f3299566f58e4dc18a4769c05bf53d1f1188
Randy Moya <stevenmoya02@gmail.com>
2024-02-08 15:33:14 -0600

    Se agrego TaskContext para evitar tener muchas props

--- src/App/appUI.js
+++ src/App/appUI.js
@@ -7,45 +7,41 @@ import { TasksTitle } from '../TasksTitle';
 import { TasksLoading } from '../TasksLoading';
 import { TasksError } from '../TasksError'
 import { EmptyTasks } from '../EmptyTasks';
+import { TasksContext } from '../TasksContext';
 
-function AppUI({
-  tasksCompleted,
-  tasksTotal,
-  searchValue,
-  setSearchValue,
-  searchedTasks,
-  finishTask,
-  deleteTask,
-  loading,
-  error
-}){
+function AppUI(){
   return (
     // crea un elemento invisible 
     <>
-
       <TasksTitle/>
-      <TasksCounter completed={tasksCompleted} total={tasksTotal} />
-      <TasksSearch
-        searchVal={searchValue} // Se le puede colocar el mismo nombre, Ej: searchValue={searchValue}
-        setSearchVal={setSearchValue}
-      />
+      <TasksCounter />
+      <TasksSearch />
 
-      <TasksList>
-        {loading && <TasksLoading/>}
-        {error && <TasksError/>}
-        {(!loading && searchedTasks.length === 0) && <EmptyTasks/>}
+      <TasksContext.Consumer>
+        {({
+          searchedTasks,
+          finishTask,
+          deleteTask,
+          loading,
+          error
+        })=>(<TasksList>
+          {loading && <TasksLoading/>}
+          {error && <TasksError/>}
+          {(!loading && searchedTasks.length === 0) && <EmptyTasks/>}
 
-        {searchedTasks.map(task => (
-          // necesario enviar lel atributo key
-          <TaskItem
-            key={task.text} 
-            text={task.text} 
-            completed={task.completed}
-            onCompleted={() => finishTask(task.text)}
-            onDelete={() => deleteTask(task.text)}
-          />
-        ))}
-      </TasksList>
+          {searchedTasks.map(task => (
+            // necesario enviar lel atributo key
+            <TaskItem
+              key={task.text} 
+              text={task.text} 
+              completed={task.completed}
+              onCompleted={() => finishTask(task.text)}
+              onDelete={() => deleteTask(task.text)}
+            />
+          ))}
+        </TasksList>)}
+      </TasksContext.Consumer>
+        
 
       <CreateTaskButton />
     </>
--- src/App/index.js
+++ src/App/index.js
@@ -1,60 +1,14 @@
 import React from 'react';
-import { useLocalStorage } from './useLocalStorage';
+import { TasksProvider } from '../TasksContext';
 import { AppUI } from './appUI';
 
 
 function App() {
-
-  // Estado de las tasks
-  const {
-    item: tasks, 
-    saveItem: saveTasks,
-    loading,
-    error,
-  } = useLocalStorage('TASKS_V1', []);
-
-  // Estado del componente TasksSearch
-  const [searchValue, setSearchValue] = React.useState('');
-  
-
-  // Estados derivados
-  const tasksCompleted = tasks.filter(task => task.completed).length;
-  const tasksTotal = tasks.length;
-
-  const searchedTasks = tasks.filter(task => task.text.toLowerCase().includes(searchValue.toLowerCase()));
-
-  // funcionalidad para marcar una task como completada
-  const finishTask = (textKey) => {
-    const newTasks = [...tasks];
-    const taskIndex = newTasks.findIndex(task => task.text === textKey)
-    
-    if(newTasks[taskIndex].completed){
-      newTasks[taskIndex].completed = false;
-    }else{
-      newTasks[taskIndex].completed = true;
-    }
-    saveTasks(newTasks);
-  }
-
-  const deleteTask = (textKey) => {
-    const newTasks = [...tasks];
-    const taskIndex = newTasks.findIndex(task => task.text === textKey)
-    newTasks.splice(taskIndex, 1);
-    saveTasks(newTasks);
-  }
-  
   return(
-    <AppUI
-    tasksCompleted = {tasksCompleted}
-    tasksTotal = {tasksTotal}
-    searchValue = {searchValue}
-    setSearchValue = {setSearchValue}
-    searchedTasks = {searchedTasks}
-    finishTask = {finishTask}
-    deleteTask = {deleteTask}
-    loading = {loading}
-    error = {error}
-  />
+    <TasksProvider>
+      <AppUI />
+    </TasksProvider>
+    
   )
 }
 
