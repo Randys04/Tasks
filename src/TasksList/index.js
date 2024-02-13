3c25aba150eaab7bef340334e3388b1273ab6bef
Randy Moya <stevenmoya02@gmail.com>
2024-02-12 22:23:31 -0600

    Mejora en el diseno reponsive

--- src/TasksList/index.js
+++ src/TasksList/index.js
@@ -3,7 +3,6 @@ import './TasksList.css'
 function TasksList({children, loading, error, emptyTasks}){
   return(
     <ul className={`${loading || error || emptyTasks ? 'without-items' : ''}`}>
-      {console.log(`${loading} ${error} ${emptyTasks}`)}
       {children}
     </ul>
   );
